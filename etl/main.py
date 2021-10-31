d416d859a5e9008b363853b43702c13629baddca
Oeitham <sirematthieu@gmail.com>
2021-10-31 16:49:28 -0400

    Added categories to restaurants

--- etl/main.py
+++ etl/main.py
@@ -14,16 +14,20 @@ HASH = "hash"
 MESSAGE = "message"
 
 DATAPATH = "/data/"
+CATEGORIESDATAPATH = "/data/categories/"
 mongo_client = MongoClient(host="mongo_service")
 log_collection_pointer = mongo_client['7035Projet']['log']
 restaurants_collection_pointer = mongo_client['7035Projet']['restaurants']
 
+CATEGORIES = {}
 
 def get_source_file(data_source_path: str) -> Tuple[str, list]:
     for file in listdir(data_source_path):
-        file_handle = open(path.join(data_source_path, file))
-        yield file, file_handle
-
+        try:
+            file_handle = open(path.join(data_source_path, file))
+            yield file, file_handle
+        except Exception as e:
+            print("Erreur avec le document {}: {}".format(file, e.args[0]))
 
 def get_log_document_template():
     return {
@@ -54,13 +58,29 @@ def insert_log(log_document: dict):
 
 def insert_restaurant(source_file: str, restaurant_document : dict):
     restaurant_document[FILENAME] = source_file
+    categoriesList = restaurant_document["Categories"].split(",")
+    categoriesListClear = []
+    for category in categoriesList:
+        categoriesListClear.append(CATEGORIES[int(category)])
+    restaurant_document["CategoriesList"] = categoriesListClear
     restaurants_collection_pointer.insert_one(restaurant_document)
 
 def delete_restaurant(source_file: str):
     restaurants_collection_pointer.delete_many({FILENAME:source_file})
 
+def load_categories():
+    for filename, file_handle in get_source_file(CATEGORIESDATAPATH):
+        try:
+            categories_document = json.load(file_handle)
+            for category in categories_document:
+                id = category['ID']
+                name = category['Nom']
+                CATEGORIES[int(id)] = name
+        except Exception as e:
+            print("Erreur avec le document {}: {}".format(filename, e.args[0]))
 
 if __name__ == "__main__":
+    load_categories()
     for filename, file_handle in get_source_file(DATAPATH):
 
         log_document = get_log_document_template()
