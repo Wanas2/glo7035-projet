85c08a5d1a316ed866cc3a6fb759e5e389cbacdd
Oeitham <sirematthieu@gmail.com>
2021-10-31 15:23:01 -0400

    extracted_data returns nbRestaurants

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
+from pymongo import MongoClient, DESCENDING
 from flask import Flask, jsonify
 import os
 import json
 
 flask_env = os.getenv('flask_env')
 application = Flask('test_app')
+
+mongo_client = MongoClient(host="mongo_service")
+log_collection_pointer = mongo_client['7035Projet']['log']
+restaurants_collection_pointer = mongo_client['7035Projet']['restaurants']
 
 if flask_env == "prod":
     debug = False
@@ -22,6 +27,13 @@ def index():
 def heartbeat():
     return jsonify({
         "villeChoisie" : "Sherbrooke"
+    })
+
+@application.route('/extracted_data')
+def extracted_data():
+    nb_restaurants = restaurants_collection_pointer.count()
+    return jsonify({
+        "nbRestaurants" : nb_restaurants
     })
 
 application.run('0.0.0.0',port, debug=debug)
