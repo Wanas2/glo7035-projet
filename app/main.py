1fdcb3e6e5c0e05cbb6f80a48cbc2a62b356dbf1
Oeitham <sirematthieu@gmail.com>
2021-10-31 21:50:32 -0400

    transformed_data returns number of restaurants per category

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient, DESCENDING
 from flask import Flask, jsonify
 import os
 import json
+from typing import Tuple
 
 flask_env = os.getenv('flask_env')
 application = Flask('test_app')
@@ -34,6 +35,23 @@ def extracted_data():
     nb_restaurants = restaurants_collection_pointer.count()
     return jsonify({
         "nbRestaurants" : nb_restaurants
+    })
+
+def get_restaurants_per_category() -> Tuple[str, int]:
+    categoriesDistinct = restaurants_collection_pointer.distinct('CategoriesList')
+    for category in categoriesDistinct:
+        nbRestaurants = restaurants_collection_pointer.count({'CategoriesList': category})
+        yield category, nbRestaurants
+
+@application.route('/transformed_data')
+def transformed_data():
+
+    categoriesDict = {}
+    for category, n in get_restaurants_per_category():
+        categoriesDict[category] = n
+
+    return jsonify({
+        "restaurants" : categoriesDict
     })
 
 application.run('0.0.0.0',port, debug=debug)
