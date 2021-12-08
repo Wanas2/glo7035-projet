14ddda7a659f3abd8e77d666af2fea7362b165d9
Haiying Li <haiying.li.1@ulaval.ca>
2021-12-08 17:29:07 -0500

    query function might come in useful for upcoming neo4j operations

--- etl/neo4j_etl.py
+++ etl/neo4j_etl.py
@@ -6,10 +6,29 @@ import json
 class Neo4jClient:
 
     def __init__(self, uri, auth):
-        self.driver = GraphDatabase.driver(uri, auth=auth)
+        self.driver=None
+        try:
+            self.driver = GraphDatabase.driver(uri, auth=auth)
+        except Exception as e:
+            print("Failed to create the driver:", e)
 
     def close(self):
-        self.driver.close()
+        if self.driver is not None:
+            self.driver.close()
+
+    def query(self, query, parameters=None, db=None):
+        assert self.driver is not None, "Driver not initialized!"
+        session = None
+        response = None
+        try: 
+            session = self.driver.session(database=db) if db is not None else self.driver.session() 
+            response = list(session.run(query, parameters))
+        except Exception as e:
+            print("Query failed:", e)
+        finally: 
+            if session is not None:
+                session.close()
+        return response
 
     def import_segments(self, data_path, file_name):
         value = self.get_json(data_path,file_name)
